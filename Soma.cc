4a7d170f77aa2ee458284a48faa8373da65fdf03
Yuri Alves <yurialves@cc.ci.ufpb.br>
2022-02-21 20:29:42 -0300

    criação do arquivo gitignore e arquivo Soma

---
+++ Soma.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int var1, var2;
+
+    var1 = 5;
+    var2 = 13;
+
+    int soma = var1 + var2;
+
+    cout << soma << endl;
+
+    return 0;
+
+
+}
